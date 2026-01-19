ef4eb8f48d4edcda58832c6f80071baa4faa4095
Gunko Vika <vikahunko@icloud.com>
2026-01-19 20:26:31 +0300

    mobile done

--- js/index.js
+++ js/index.js
@@ -5,21 +5,35 @@ const colors = {
     YELLOW: 'yellow',
     PURPLE: 'purple',
 }
-
+//массив с моковыми объектами
 const MOCK_NOTES = [
     {
         id: 1,
-        title: 'Работа с формами',
-        content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
+        title: 'Изучение JS',
+        content: 'Начинаем изучать основы. Углубляемся.',
+        color: 'yellow',
+        isFavorite: true,
+    },
+    {
+        id: 2,
+        title: 'Изучение CSS, HTML',
+        content: 'Повторяем изученное.',
         color: 'green',
         isFavorite: false,
     },
-    // ...
+    {
+        id: 1,
+        title: 'Изучение React',
+        content: 'После изучения JS переходим к React.',
+        color: 'blue',
+        isFavorite: false,
+    },
 ]
 
 const model = {
     notes: MOCK_NOTES,
 }
+console.log(model.notes.length)
 
 const view = {
     init() {
